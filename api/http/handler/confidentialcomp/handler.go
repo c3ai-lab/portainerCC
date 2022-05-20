3b6a9b386b73f54234b6bbfdc0157e2680e5ed23
Julian <julian-k69@mailbox.org>
2022-05-20 21:46:33 +0200

    Added new Handler for SGX-KeyGeneration

---
+++ api/http/handler/confidentialcomp/handler.go
@@ -0,0 +1,27 @@
+package confidentialcomp
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	httperror "github.com/portainer/libhttp/error"
+	"github.com/portainer/portainer/api/dataservices"
+	"github.com/portainer/portainer/api/http/security"
+)
+
+// Handler is the HTTP handler used to handle role operations.
+type Handler struct {
+	*mux.Router
+	DataStore dataservices.DataStore
+}
+
+// NewHandler creates a handler to manage role operations.
+func NewHandler(bouncer *security.RequestBouncer) *Handler {
+	h := &Handler{
+		Router: mux.NewRouter(),
+	}
+	h.Handle("/settings/sgx-keygen",
+		bouncer.AdminAccess(httperror.LoggerHandler(h.sgxKeyGen))).Methods(http.MethodGet)
+
+	return h
+}
