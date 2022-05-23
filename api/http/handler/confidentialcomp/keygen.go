55274e535ac5723ebf94fce7e35c47d4aac269b1
Julian <julian-k69@mailbox.org>
2022-05-23 23:24:37 +0200

    Changed GET to POST Method. New readout of variable name in Client-POST

--- api/http/handler/confidentialcomp/keygen.go
+++ api/http/handler/confidentialcomp/keygen.go
@@ -1,6 +1,7 @@
 package confidentialcomp
 
 import (
+	"errors"
 	"net/http"
 
 	httperror "github.com/portainer/libhttp/error"
@@ -19,12 +20,19 @@ import (
 // @failure 500 "Server error"
 func (handler *Handler) sgxKeyGen(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
 
-	// Keygenerierung initialisieren
-	err := handler.DataStore.ConfCompute().Create("Keyname")
+	// Read query
+	name := r.PostFormValue("name")
+
+	if name == "" {
+		return &httperror.HandlerError{http.StatusNotAcceptable, "The name parameter is missing", errors.New("Wrong syntax")}
+	}
+
+	// initialize Keygen
+	err := handler.DataStore.ConfCompute().Create(name)
 
 	if err != nil {
 		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to generate new key", err}
 	}
 
-	return response.JSON(w, err)
+	return response.JSON(w, "New key added: "+name)
 }
