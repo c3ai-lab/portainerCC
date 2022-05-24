61c1876fa6ad1e98f42fce82ded6404848311b5f
marcely0 <marcelt91@live.de>
2022-05-24 23:44:11 +0200

    simple post sgx-keygen for first step

--- app/portainer/views/confidential-computing/confidential-computing.controller.js
+++ app/portainer/views/confidential-computing/confidential-computing.controller.js
@@ -3,17 +3,39 @@ import angular from 'angular';
 angular.module('portainer.app').controller('ConfidentialComputingController', ConfidentialComputingController);
 
 /* @ngInject */
-export default function ConfidentialComputingController(Notifications){
+export default function ConfidentialComputingController(Notifications, $async, $http, $q) {
 
   var ctrl = this;
+
+  var deferred = $q.defer();
+
+  // TODO in service auslagern
 
   this.generateKey = () => {
     Notifications.success('Success', 'New SGX Signing Key created!');
     console.log("--------------------genKEy");
+    $async(async () => {
+      $http.post('https://localhost:9443/api/settings/sgx-keygen', { name: 'superGeilerSigninKey' }).then(function success(data) {
+        console.log("ja moin");
+        console.log(data);
+      }).catch(function error(err) {
+        console.log("ERRORRRRRRRRRRRRRRR");
+      })
+      console.log("async");
+    })
   }
 
   this.importKey = () => {
-    Notifications.success('Success', 'New SGX Signing Key imported!');
+    Notifications.error('Failure', 'No import implemented yet!');
     console.log("-------------------------importkey");
+    $async(async () => {
+      $http.get('https://localhost:9443/api/users').then(function success(data) {
+        console.log("ja moin");
+        console.log(data);
+      }).catch(function error(err) {
+        console.log("ERRORRRRRRRRRRRRRRR");
+      })
+      console.log("async");
+    })
   }
 }
