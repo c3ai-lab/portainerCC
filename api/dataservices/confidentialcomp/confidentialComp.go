d3e935dc46e42a54f7245f09e3d91c6f57ba1546
Julian <julian-k69@mailbox.org>
2022-05-25 00:02:30 +0200

    added key generation

--- api/dataservices/confidentialcomp/confidentialComp.go
+++ api/dataservices/confidentialcomp/confidentialComp.go
@@ -1,6 +1,11 @@
 package confidentialcomp
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"errors"
+	"fmt"
+
 	portainer "github.com/portainer/portainer/api"
 )
 
@@ -31,10 +36,27 @@ func NewService(connection portainer.Connection) (*Service, error) {
 
 // CreateKey creates a new private Key
 func (service *Service) Create(name string) error {
+
+	// generate new rsa key
+	privatekey, err := rsa.GenerateKey(rand.Reader, 3072)
+	if err != nil {
+		fmt.Printf("Cannot generate RSA key\n")
+		return errors.New("Could not generate Key")
+	}
+
+	keyObject := portainer.ConfCompute{
+		0,
+		name,
+		privatekey,
+	}
+
+	fmt.Printf(privatekey.N.String())
+
 	return service.connection.CreateObject(
 		BucketName,
 		func(id uint64) (int, interface{}) {
-			return int(id), name
+			keyObject.ID = portainer.ConfComputeID(id)
+			return int(id), keyObject
 		},
 	)
 }
