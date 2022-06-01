e01ecce4304485d8865f15586afa1239923470c5
André Lorenzen <andrelorenzen@gmx.de>
2022-06-01 15:56:52 +0200

    pass the signingKeyId as docker build arg

--- api/http/proxy/factory/docker/build.go
+++ api/http/proxy/factory/docker/build.go
@@ -3,15 +3,19 @@ package docker
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/portainer/portainer/api/archive"
 )
 
 type postDockerfileRequest struct {
-	Content string
+	Content      string
+	SigningKeyId int64
 }
 
 // buildOperation inspects the "Content-Type" header to determine if it needs to alter the request.
@@ -23,6 +27,19 @@ type postDockerfileRequest struct {
 // In any other case, it will leave the request unaltered.
 func buildOperation(request *http.Request) error {
 	contentTypeHeader := request.Header.Get("Content-Type")
+
+	if !strings.Contains(contentTypeHeader, "application/json") {
+		params, ok := request.URL.Query()["signingKeyId"]
+		if !ok || len(params) == 0 {
+			return errors.New("missing url parameter 'signingKeyId'")
+		}
+
+		signingKeyId := params[0]
+		if signingKeyId != "0" {
+			setSgxSignerKeyBuildArg(request, params[0])
+		}
+	}
+
 	if contentTypeHeader != "" && !strings.Contains(contentTypeHeader, "application/json") {
 		return nil
 	}
@@ -41,6 +58,11 @@ func buildOperation(request *http.Request) error {
 			return err
 		}
 		dockerfileContent = []byte(req.Content)
+		signingKeyId := strconv.FormatInt(req.SigningKeyId, 10)
+
+		if signingKeyId != "0" {
+			setSgxSignerKeyBuildArg(request, signingKeyId)
+		}
 	}
 
 	buffer, err := archive.TarFileInBuffer(dockerfileContent, "Dockerfile", 0600)
@@ -53,4 +75,10 @@ func buildOperation(request *http.Request) error {
 	request.Header.Set("Content-Type", "application/x-tar")
 
 	return nil
+}
+
+func setSgxSignerKeyBuildArg(request *http.Request, signingKeyId string) {
+	params := request.URL.Query()
+	params.Add("buildargs", fmt.Sprintf("{\"SGX_SIGNER_KEY\":\"key #%s was selected\"}", signingKeyId))
+	request.URL.RawQuery = params.Encode()
 }
