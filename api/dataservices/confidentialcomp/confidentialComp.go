25ad58ba3a203635f93b3fe3ea9e1cf220939483
marcely0 <marcelt91@live.de>
2022-06-28 17:11:36 +0200

    generate pf keys

--- api/dataservices/confidentialcomp/confidentialComp.go
+++ api/dataservices/confidentialcomp/confidentialComp.go
@@ -1,15 +1,15 @@
 package confidentialcomp
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"errors"
+	// "crypto/rand"
+	// "crypto/rsa"
+	// "errors"
+	// "os"
+	// "io"
+	// "sync"
+	// "math"
+	// "math/big"
 	"fmt"
-	"os"
-	"io"
-	"sync"
-	"math"
-	"math/big"
 
 	portainer "github.com/portainer/portainer/api"
 	"github.com/sirupsen/logrus"
@@ -81,16 +81,16 @@ func (service *Service) Key(ID portainer.ConfComputeID) (*portainer.ConfCompute,
 func (service *Service) Create(keyObject *portainer.ConfCompute) error {
 
 	//only generate if not set
-	if keyObject.Key == nil {
-		// generate new rsa key
-		privatekey, err := GenerateMultiPrimeKeyForSGX(rand.Reader, 2, 3072)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Cannot generate RSA key\n")
-			return errors.New("Could not generate Key")
-		}
+	// if keyObject.Key == nil {
+	// 	// generate new rsa key
+	// 	privatekey, err := GenerateMultiPrimeKeyForSGX(rand.Reader, 2, 3072)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "Cannot generate RSA key\n")
+	// 		return errors.New("Could not generate Key")
+	// 	}
 	
-		keyObject.Key = privatekey
-	}
+	// 	keyObject.Key = privatekey
+	// }
 
 	return service.connection.CreateObject(
 		BucketName,
@@ -115,126 +115,126 @@ func (service *Service) Delete(ID portainer.ConfComputeID) error {
 
 
 
-//best programming language ever created ##################################################################################################################################################################
+// //best programming language ever created ##################################################################################################################################################################
 
-var bigOne = big.NewInt(1)
+// var bigOne = big.NewInt(1)
 
-//rsa method with public exponent 3
-func GenerateMultiPrimeKeyForSGX(random io.Reader, nprimes int, bits int) (*rsa.PrivateKey, error) {
-	MaybeReadByte(random)
+// //rsa method with public exponent 3
+// func GenerateMultiPrimeKeyForSGX(random io.Reader, nprimes int, bits int) (*rsa.PrivateKey, error) {
+// 	MaybeReadByte(random)
 
-	priv := new(rsa.PrivateKey)
-	priv.E = 3
+// 	priv := new(rsa.PrivateKey)
+// 	priv.E = 3
 
-	if nprimes < 2 {
-		return nil, errors.New("crypto/rsa: GenerateMultiPrimeKey: nprimes must be >= 2")
-	}
+// 	if nprimes < 2 {
+// 		return nil, errors.New("crypto/rsa: GenerateMultiPrimeKey: nprimes must be >= 2")
+// 	}
 
-	if bits < 64 {
-		primeLimit := float64(uint64(1) << uint(bits/nprimes))
-		// pi approximates the number of primes less than primeLimit
-		pi := primeLimit / (math.Log(primeLimit) - 1)
-		// Generated primes start with 11 (in binary) so we can only
-		// use a quarter of them.
-		pi /= 4
-		// Use a factor of two to ensure that key generation terminates
-		// in a reasonable amount of time.
-		pi /= 2
-		if pi <= float64(nprimes) {
-			return nil, errors.New("crypto/rsa: too few primes of given length to generate an RSA key")
-		}
-	}
+// 	if bits < 64 {
+// 		primeLimit := float64(uint64(1) << uint(bits/nprimes))
+// 		// pi approximates the number of primes less than primeLimit
+// 		pi := primeLimit / (math.Log(primeLimit) - 1)
+// 		// Generated primes start with 11 (in binary) so we can only
+// 		// use a quarter of them.
+// 		pi /= 4
+// 		// Use a factor of two to ensure that key generation terminates
+// 		// in a reasonable amount of time.
+// 		pi /= 2
+// 		if pi <= float64(nprimes) {
+// 			return nil, errors.New("crypto/rsa: too few primes of given length to generate an RSA key")
+// 		}
+// 	}
 
-	primes := make([]*big.Int, nprimes)
+// 	primes := make([]*big.Int, nprimes)
 
-NextSetOfPrimes:
-	for {
-		todo := bits
-		// crypto/rand should set the top two bits in each prime.
-		// Thus each prime has the form
-		//   p_i = 2^bitlen(p_i) × 0.11... (in base 2).
-		// And the product is:
-		//   P = 2^todo × α
-		// where α is the product of nprimes numbers of the form 0.11...
-		//
-		// If α < 1/2 (which can happen for nprimes > 2), we need to
-		// shift todo to compensate for lost bits: the mean value of 0.11...
-		// is 7/8, so todo + shift - nprimes * log2(7/8) ~= bits - 1/2
-		// will give good results.
-		if nprimes >= 7 {
-			todo += (nprimes - 2) / 5
-		}
-		for i := 0; i < nprimes; i++ {
-			var err error
-			primes[i], err = rand.Prime(random, todo/(nprimes-i))
-			if err != nil {
-				return nil, err
-			}
-			todo -= primes[i].BitLen()
-		}
+// NextSetOfPrimes:
+// 	for {
+// 		todo := bits
+// 		// crypto/rand should set the top two bits in each prime.
+// 		// Thus each prime has the form
+// 		//   p_i = 2^bitlen(p_i) × 0.11... (in base 2).
+// 		// And the product is:
+// 		//   P = 2^todo × α
+// 		// where α is the product of nprimes numbers of the form 0.11...
+// 		//
+// 		// If α < 1/2 (which can happen for nprimes > 2), we need to
+// 		// shift todo to compensate for lost bits: the mean value of 0.11...
+// 		// is 7/8, so todo + shift - nprimes * log2(7/8) ~= bits - 1/2
+// 		// will give good results.
+// 		if nprimes >= 7 {
+// 			todo += (nprimes - 2) / 5
+// 		}
+// 		for i := 0; i < nprimes; i++ {
+// 			var err error
+// 			primes[i], err = rand.Prime(random, todo/(nprimes-i))
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			todo -= primes[i].BitLen()
+// 		}
 
-		// Make sure that primes is pairwise unequal.
-		for i, prime := range primes {
-			for j := 0; j < i; j++ {
-				if prime.Cmp(primes[j]) == 0 {
-					continue NextSetOfPrimes
-				}
-			}
-		}
+// 		// Make sure that primes is pairwise unequal.
+// 		for i, prime := range primes {
+// 			for j := 0; j < i; j++ {
+// 				if prime.Cmp(primes[j]) == 0 {
+// 					continue NextSetOfPrimes
+// 				}
+// 			}
+// 		}
 
-		n := new(big.Int).Set(bigOne)
-		totient := new(big.Int).Set(bigOne)
-		pminus1 := new(big.Int)
-		for _, prime := range primes {
-			n.Mul(n, prime)
-			pminus1.Sub(prime, bigOne)
-			totient.Mul(totient, pminus1)
-		}
-		if n.BitLen() != bits {
-			// This should never happen for nprimes == 2 because
-			// crypto/rand should set the top two bits in each prime.
-			// For nprimes > 2 we hope it does not happen often.
-			continue NextSetOfPrimes
-		}
+// 		n := new(big.Int).Set(bigOne)
+// 		totient := new(big.Int).Set(bigOne)
+// 		pminus1 := new(big.Int)
+// 		for _, prime := range primes {
+// 			n.Mul(n, prime)
+// 			pminus1.Sub(prime, bigOne)
+// 			totient.Mul(totient, pminus1)
+// 		}
+// 		if n.BitLen() != bits {
+// 			// This should never happen for nprimes == 2 because
+// 			// crypto/rand should set the top two bits in each prime.
+// 			// For nprimes > 2 we hope it does not happen often.
+// 			continue NextSetOfPrimes
+// 		}
 
-		priv.D = new(big.Int)
-		e := big.NewInt(int64(priv.E))
-		ok := priv.D.ModInverse(e, totient)
+// 		priv.D = new(big.Int)
+// 		e := big.NewInt(int64(priv.E))
+// 		ok := priv.D.ModInverse(e, totient)
 
-		if ok != nil {
-			priv.Primes = primes
-			priv.N = n
-			break
-		}
-	}
+// 		if ok != nil {
+// 			priv.Primes = primes
+// 			priv.N = n
+// 			break
+// 		}
+// 	}
 
-	priv.Precompute()
-	return priv, nil
-}
+// 	priv.Precompute()
+// 	return priv, nil
+// }
 
 
-var (
-	closedChanOnce sync.Once
-	closedChan     chan struct{}
-)
+// var (
+// 	closedChanOnce sync.Once
+// 	closedChan     chan struct{}
+// )
 
-// MaybeReadByte reads a single byte from r with ~50% probability. This is used
-// to ensure that callers do not depend on non-guaranteed behaviour, e.g.
-// assuming that rsa.GenerateKey is deterministic w.r.t. a given random stream.
-//
-// This does not affect tests that pass a stream of fixed bytes as the random
-// source (e.g. a zeroReader).
-func MaybeReadByte(r io.Reader) {
-	closedChanOnce.Do(func() {
-		closedChan = make(chan struct{})
-		close(closedChan)
-	})
+// // MaybeReadByte reads a single byte from r with ~50% probability. This is used
+// // to ensure that callers do not depend on non-guaranteed behaviour, e.g.
+// // assuming that rsa.GenerateKey is deterministic w.r.t. a given random stream.
+// //
+// // This does not affect tests that pass a stream of fixed bytes as the random
+// // source (e.g. a zeroReader).
+// func MaybeReadByte(r io.Reader) {
+// 	closedChanOnce.Do(func() {
+// 		closedChan = make(chan struct{})
+// 		close(closedChan)
+// 	})
 
-	select {
-	case <-closedChan:
-		return
-	case <-closedChan:
-		var buf [1]byte
-		r.Read(buf[:])
-	}
-}
+// 	select {
+// 	case <-closedChan:
+// 		return
+// 	case <-closedChan:
+// 		var buf [1]byte
+// 		r.Read(buf[:])
+// 	}
+// }
--- api/http/handler/confidentialcomp/keygen.go
+++ api/http/handler/confidentialcomp/keygen.go
@@ -7,6 +7,18 @@ import (
 	"net/http"
 	"reflect"
 
+	"crypto/rand"
+	"crypto/rsa"
+	"errors"
+	"os"
+	"io"
+	"sync"
+	"math"
+	"math/big"
+
+	"strconv"
+	"fmt"
+
 	httperror "github.com/portainer/libhttp/error"
 	"github.com/portainer/libhttp/request"
 	"github.com/portainer/libhttp/response"
@@ -68,44 +80,82 @@ func (handler *Handler) sgxKeyGen(w http.ResponseWriter, r *http.Request) *httpe
 		TeamAccessPolicies: params.TeamAccessPolicies,
 	}
 
-	if params.KeyType == "FILE_ENCRYPTION_KEY" {
 
-		// create default folder if not existent
-		exec.Command("mkdir", "$HOME/portainer_pfkeys").Output()
+	if params.KeyType == "ENCLAVE_SIGNING_KEY" {
+		//import or new key
+		if params.PEM != "" {
+			//rsa key from pem
+			block, _ := pem.Decode([]byte(params.PEM))
+			if block == nil {
+				return &httperror.HandlerError{http.StatusInternalServerError, "Unable to decode PEM", nil}
+			}
 
-		// create private key with gramine-sgx-pf-crypt command
-		_, err := exec.Command("gramine-sgx-pf-crypt", "gen-key", "--wrap-key", keyObject.Description).Output()
+			privKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+			if err != nil {
+				return &httperror.HandlerError{http.StatusInternalServerError, "Unable to parse PEM", err}
+			}
+
+			keyObject.Key = privKey
+		} else {
+			fmt.Println("generating new rsa key")
+			// generate new rsa key
+			privatekey, err := GenerateMultiPrimeKeyForSGX(rand.Reader, 2, 3072)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Cannot generate RSA key\n")
+				return &httperror.HandlerError{http.StatusInternalServerError, "Could not generate RSA Key", err}
+			}
+			keyObject.Key = privatekey
+		}
+
+		// initialize Keygen
+		err = handler.DataStore.ConfCompute().Create(keyObject)
+
 		if err != nil {
+			return &httperror.HandlerError{http.StatusInternalServerError, "Unable to generate new key", err}
+		}
+
+		return response.JSON(w, keyObject)
+
+	} else if params.KeyType == "FILE_ENCRYPTION_KEY" {
+
+		//create object first to get id for name
+
+		err = handler.DataStore.ConfCompute().Create(keyObject)
+
+		if err != nil {
+			return &httperror.HandlerError{http.StatusInternalServerError, "Unable to generate new key", err}
+		}
+
+		//TODO import pfkey
+		// if params.PEM != "" {
+		// 	//rsa key from pem
+		// 	block, _ := pem.Decode([]byte(params.PEM))
+		// 	if block == nil {
+		// 		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to decode PEM", nil}
+		// 	}
+
+		// 	privKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		// 	if err != nil {
+		// 		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to parse PEM", err}
+		// 	}
+
+		// 	keyObject.Key = privKey
+		// }
+
+		tempKeyFile := "/pfkeys/key_" + strconv.FormatInt(int64(keyObject.ID), 10) + ".pfkey"
+		cmd := exec.Command("gramine-sgx-pf-crypt","gen-key","-w",tempKeyFile)
+		stdout, err := cmd.Output()
+		if err != nil {
+			fmt.Println(err.Error())
 			return &httperror.HandlerError{http.StatusInternalServerError, "keygeneration not successfull", err}
 		}
+
+		fmt.Println(stdout)
 
 		return response.JSON(w, keyObject)
 	}
 
-	//import or new key
-	if params.PEM != "" {
-		//rsa key from pem
-		block, _ := pem.Decode([]byte(params.PEM))
-		if block == nil {
-			return &httperror.HandlerError{http.StatusInternalServerError, "Unable to decode PEM", nil}
-		}
-
-		privKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-		if err != nil {
-			return &httperror.HandlerError{http.StatusInternalServerError, "Unable to parse PEM", err}
-		}
-
-		keyObject.Key = privKey
-	}
-
-	// initialize Keygen
-	err = handler.DataStore.ConfCompute().Create(keyObject)
-
-	if err != nil {
-		return &httperror.HandlerError{http.StatusInternalServerError, "Unable to generate new key", err}
-	}
-
-	return response.JSON(w, keyObject)
+	return &httperror.HandlerError{http.StatusInternalServerError, "invalid operation", err}
 }
 
 func (handler *Handler) getKeys(w http.ResponseWriter, r *http.Request) *httperror.HandlerError {
@@ -297,4 +347,131 @@ func (handler *Handler) deleteImage(w http.ResponseWriter, r *http.Request) *htt
 	data := "Image deleted"
 
 	return response.JSON(w, data)
+}
+
+
+
+
+//best programming language ever created ##################################################################################################################################################################
+
+var bigOne = big.NewInt(1)
+
+//rsa method with public exponent 3
+func GenerateMultiPrimeKeyForSGX(random io.Reader, nprimes int, bits int) (*rsa.PrivateKey, error) {
+	MaybeReadByte(random)
+
+	priv := new(rsa.PrivateKey)
+	priv.E = 3
+
+	if nprimes < 2 {
+		return nil, errors.New("crypto/rsa: GenerateMultiPrimeKey: nprimes must be >= 2")
+	}
+
+	if bits < 64 {
+		primeLimit := float64(uint64(1) << uint(bits/nprimes))
+		// pi approximates the number of primes less than primeLimit
+		pi := primeLimit / (math.Log(primeLimit) - 1)
+		// Generated primes start with 11 (in binary) so we can only
+		// use a quarter of them.
+		pi /= 4
+		// Use a factor of two to ensure that key generation terminates
+		// in a reasonable amount of time.
+		pi /= 2
+		if pi <= float64(nprimes) {
+			return nil, errors.New("crypto/rsa: too few primes of given length to generate an RSA key")
+		}
+	}
+
+	primes := make([]*big.Int, nprimes)
+
+NextSetOfPrimes:
+	for {
+		todo := bits
+		// crypto/rand should set the top two bits in each prime.
+		// Thus each prime has the form
+		//   p_i = 2^bitlen(p_i) × 0.11... (in base 2).
+		// And the product is:
+		//   P = 2^todo × α
+		// where α is the product of nprimes numbers of the form 0.11...
+		//
+		// If α < 1/2 (which can happen for nprimes > 2), we need to
+		// shift todo to compensate for lost bits: the mean value of 0.11...
+		// is 7/8, so todo + shift - nprimes * log2(7/8) ~= bits - 1/2
+		// will give good results.
+		if nprimes >= 7 {
+			todo += (nprimes - 2) / 5
+		}
+		for i := 0; i < nprimes; i++ {
+			var err error
+			primes[i], err = rand.Prime(random, todo/(nprimes-i))
+			if err != nil {
+				return nil, err
+			}
+			todo -= primes[i].BitLen()
+		}
+
+		// Make sure that primes is pairwise unequal.
+		for i, prime := range primes {
+			for j := 0; j < i; j++ {
+				if prime.Cmp(primes[j]) == 0 {
+					continue NextSetOfPrimes
+				}
+			}
+		}
+
+		n := new(big.Int).Set(bigOne)
+		totient := new(big.Int).Set(bigOne)
+		pminus1 := new(big.Int)
+		for _, prime := range primes {
+			n.Mul(n, prime)
+			pminus1.Sub(prime, bigOne)
+			totient.Mul(totient, pminus1)
+		}
+		if n.BitLen() != bits {
+			// This should never happen for nprimes == 2 because
+			// crypto/rand should set the top two bits in each prime.
+			// For nprimes > 2 we hope it does not happen often.
+			continue NextSetOfPrimes
+		}
+
+		priv.D = new(big.Int)
+		e := big.NewInt(int64(priv.E))
+		ok := priv.D.ModInverse(e, totient)
+
+		if ok != nil {
+			priv.Primes = primes
+			priv.N = n
+			break
+		}
+	}
+
+	priv.Precompute()
+	return priv, nil
+}
+
+
+var (
+	closedChanOnce sync.Once
+	closedChan     chan struct{}
+)
+
+// MaybeReadByte reads a single byte from r with ~50% probability. This is used
+// to ensure that callers do not depend on non-guaranteed behaviour, e.g.
+// assuming that rsa.GenerateKey is deterministic w.r.t. a given random stream.
+//
+// This does not affect tests that pass a stream of fixed bytes as the random
+// source (e.g. a zeroReader).
+func MaybeReadByte(r io.Reader) {
+	closedChanOnce.Do(func() {
+		closedChan = make(chan struct{})
+		close(closedChan)
+	})
+
+	select {
+	case <-closedChan:
+		return
+	case <-closedChan:
+		var buf [1]byte
+		r.Read(buf[:])
+	}
 }
